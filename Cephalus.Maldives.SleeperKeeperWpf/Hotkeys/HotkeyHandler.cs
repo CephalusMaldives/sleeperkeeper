ddc5fa99833e51d3eea29ed767430d83d00b649b
Jovo Grbic <Jovo.Grbic@health.telstra.com>
2019-03-29 16:07:52 +0000

    Added a bunch of stuff + tray icon

---
+++ Cephalus.Maldives.SleeperKeeperWpf/Hotkeys/HotkeyHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Cephalus.Maldives.SleeperKeeperWpf.Hotkeys
+{
+	public class HotkeyHandler
+	{
+		[DllImport("User32.dll")]
+		private static extern bool RegisterHotKey(
+			[In] IntPtr hWnd,
+			[In] int id,
+			[In] uint fsModifiers,
+			[In] uint vk);
+
+		[DllImport("User32.dll")]
+		private static extern bool UnregisterHotKey(
+			[In] IntPtr hWnd,
+			[In] int id);
+
+		private const int HOTKEY_ID = 9000;
+
+		private readonly HwndSource _source;
+		private readonly Window _window;
+		private Action _handler;
+
+		public HotkeyHandler(Window window)
+		{
+			_window = window;
+
+			var helper = new WindowInteropHelper(window);
+
+			_source = HwndSource.FromHwnd(helper.Handle);
+			_source.AddHook(HwndHook);
+
+			RegisterHotKeyInternal();
+		}
+
+		public void RegisterKeyHandler(Action handler)
+		{
+			_handler = handler;
+		}
+
+		public void UnregisterHotKey()
+		{
+			UnregisterHotKeyInternal();
+		}
+
+		private void UnregisterHotKeyInternal()
+		{
+			var helper = new WindowInteropHelper(_window);
+
+			UnregisterHotKey(helper.Handle, HOTKEY_ID);
+		}
+
+		private void RegisterHotKeyInternal()
+		{
+			var helper = new WindowInteropHelper(_window);
+
+			const uint VK_F10 = 0x79;
+			const uint MOD_CTRL = 0x0002;
+
+			if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_CTRL, VK_F10))
+			{
+				// handle error
+			}
+		}
+
+		private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			const int WM_HOTKEY = 0x0312;
+
+			switch (msg)
+			{
+				case WM_HOTKEY:
+					switch (wParam.ToInt32())
+					{
+						case HOTKEY_ID:
+							if (_handler != null)
+							{
+								_handler();
+							}
+
+							handled = true;
+							break;
+					}
+					break;
+			}
+
+			return IntPtr.Zero;
+		}
+	}
+}
--- Cephalus.Maldives.SleeperKeeperWpf/MainWindow.xaml.cs
+++ Cephalus.Maldives.SleeperKeeperWpf/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
+﻿using Cephalus.Maldives.SleeperKeeperWpf.Hotkeys;
+using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Threading;
@@ -13,30 +13,86 @@ namespace Cephalus.Maldives.SleeperKeeperWpf
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		//[DllImport("user32.dll")]
-		//static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+		private const int DefaultIntervalInMilliseconds = 1000;
 
-		//[DllImport("user32.dll")]
-		//static extern bool SendInput(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
-
-		//const uint WM_KEYDOWN = 0x0100;
-		//const int VK_F15 = 0x7e;
-
-		private readonly DispatcherTimer _timer;
 		private int _counter;
+		private DispatcherTimer _timer;
 		private TimeSpan _activeTime;
 		private InputSimulator _simulator;
+		private static HotkeyHandler _hotkeyHandler;
+		private NotifyIcon _notifyIcon;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			_timer = new DispatcherTimer();
-			_timer.Interval = new TimeSpan(0, 0, 0, 1);
-			_timer.Tick += _timer_Tick;
-			_timer.Start();
+			_simulator = new InputSimulator();
+
+			_notifyIcon = new NotifyIcon();
+			_notifyIcon.Visible = true;
+			_notifyIcon.Icon = new System.Drawing.Icon(@"C:\Users\jgribic\Desktop\icon.ico");
+			_notifyIcon.DoubleClick += _notifyIcon_DoubleClick;
+			_notifyIcon.ShowBalloonTip(5000, "Hi I am right here", "Hi I am right now", ToolTipIcon.Info);
+
+			VisualTextRenderingMode = System.Windows.Media.TextRenderingMode.Grayscale;
+
+			SetupTimer();
 
 			_activeTime = new TimeSpan();
+		}
+
+		private void _notifyIcon_DoubleClick(object sender, EventArgs e)
+		{
+			if (WindowState == WindowState.Minimized)
+			{
+				WindowState = WindowState.Normal;
+			}
+			else
+			{
+				WindowState = WindowState.Minimized;
+			}
+		}
+
+		private void SetupTimer()
+		{
+			_timer = new DispatcherTimer
+			{
+				Interval = new TimeSpan(0, 0, 0, 1)
+			};
+			_timer.Tick += _timer_Tick;
+			_timer.Start();
+		}
+
+		protected override void OnActivated(EventArgs e)
+		{
+			base.OnActivated(e);
+
+			if (_hotkeyHandler == null)
+			{
+				_hotkeyHandler = new HotkeyHandler(this);
+				_hotkeyHandler.RegisterKeyHandler(() => 
+				{
+					if (WindowState == WindowState.Minimized)
+					{
+						WindowState = WindowState.Normal;
+						ShowActivated = true;
+						Show();
+						Activate();
+					}
+					else
+					{
+						WindowState = WindowState.Minimized;
+						Hide();
+					}
+				});
+			}
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			base.OnClosing(e);
+			_hotkeyHandler.UnregisterHotKey();
+			_notifyIcon.Dispose();
 		}
 
 		private void TrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -46,21 +102,44 @@ namespace Cephalus.Maldives.SleeperKeeperWpf
 
 		private void _timer_Tick(object sender, EventArgs e)
 		{
-			//var processes = Process.GetProcessesByName("explorer");
-			//var success = false;
+			try
+			{
+				_simulator.Mouse.MoveMouseBy(1, 1).MoveMouseBy(-1, -1);
+				_activeTime = _activeTime.Add(TimeSpan.FromMilliseconds(_timer.Interval.TotalMilliseconds));
+				lblContent.Text = $"Preventing sleep {_counter++} times over an interval of {_timer.Interval.Seconds} seconds.\n\rSystem is active for {_activeTime.TotalSeconds} seconds.";
+			}
+			catch
+			{
 
-			//foreach (var process in processes)
-			//{
-			//	success = PostMessage(process.MainWindowHandle, WM_KEYDOWN, VK_F15, 0);
-			//}
-
-			_activeTime = _activeTime.Add(TimeSpan.FromSeconds(1));
-			lblContent.Text = $"Sent F15 key to prevent sleep {_counter++} times over an interval of {_timer.Interval.Seconds} seconds. System active for {_activeTime.TotalSeconds} seconds with {success}";
+			}
 		}
 
 		private void ButtonExit_Click(object sender, RoutedEventArgs e)
 		{
 			System.Windows.Application.Current.Shutdown();
+		}
+
+		private void TxtInterval_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key == System.Windows.Input.Key.Enter)
+			{
+				if (!int.TryParse(txtInterval.Text, out int interval))
+				{
+					interval = DefaultIntervalInMilliseconds;
+					txtInterval.Text = interval.ToString();
+				}
+
+				e.Handled = true;
+				_timer.Interval = new TimeSpan(0, 0, 0, 0, interval);
+			}
+		}
+
+		private void TxtInterval_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key == System.Windows.Input.Key.Enter)
+			{
+				e.Handled = true;
+			}
 		}
 	}
 }
